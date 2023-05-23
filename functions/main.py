409a2d76d76ec04393608606e0ac03dd4c796f90
Toa Carney <toa@toacarney.nz>
2023-05-23 16:20:12 +1200

    add python cloud function

--- functions/main.py
+++ functions/main.py
@@ -4,10 +4,13 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+import numpy as np
+import pandas as pd
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+
+
+@https_fn.on_request(region="australia-southeast1")
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    x = np.random.randn(10,10)
+    return https_fn.Response(np.array2string(x))
